7c06ed350c89e0288f52bdbc7ed8e1eea72407a5
kuba-rosiek <rosiek.kuba@gmail.com>
2017-07-18 11:15:55 +0100

    Update gruntfile.js
    Usunięte browserSync.

--- gruntfile.js
+++ gruntfile.js
@@ -33,25 +33,14 @@ module.exports = function(grunt) {
 			},
 		}
 	},
-	browserSync: {
-		bsFiles: {
-			src : 'css/*.css'
-		},
-		options: {
-			server: {
-				baseDir: "./",
-			}
-		}
-	}
   });
   // Load the plugins tasks
   grunt.loadNpmTasks('grunt-contrib-watch');
   grunt.loadNpmTasks('grunt-sass');
   grunt.loadNpmTasks('grunt-contrib-imagemin');
-  grunt.loadNpmTasks('grunt-browser-sync');
   // Default task(s).
 
-  grunt.registerTask('default', ['sass', 'browserSync', 'watch']);
+  grunt.registerTask('default', ['sass', 'watch']);
   grunt.registerTask('images', ['imagemin']);
   
   
